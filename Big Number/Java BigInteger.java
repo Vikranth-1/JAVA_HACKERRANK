c894b3354da737ff184e2ff465fe54578c6cd84a
Vikranth V <vvikranth08@gmail.com>
2026-03-14 08:54:59 +0530

    Create Java BigInteger.java

---
+++ Big Number/Java BigInteger.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+        scanner.close();
+        
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
