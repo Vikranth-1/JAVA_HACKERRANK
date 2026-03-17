7700ead359b4dbd3d78c4b52d58203c7d9712c7b
Vikranth V <vvikranth08@gmail.com>
2026-03-18 00:33:02 +0530

    Create Java Interface.java

---
+++ Object Oriented Programming/Java Interface.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+
+    public int divisor_sum(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        
+        Class<?>[] interfaces = my_calculator.getClass().getInterfaces();
+        for (Class<?> i : interfaces) {
+            System.out.println(i.getName());
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(my_calculator.divisor_sum(n));
+        
+        sc.close();
+    }
+}
